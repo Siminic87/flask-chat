f1f6dd550f9b0846aae4bf8434897ff68d0ddf63
Simon O <oeltzschner@hotmail.com>
2018-11-01 10:28:22 +0000

    Add ability write to, and read from a list

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
+
+def add_messages(username, message):
+    """Add messages to the 'messages list'"""
+    messages.append("{}: {}".format(username, message))
+    
+def get_all_messages():
+    """Get all of the messages and separate them by a 'br'"""
+    return "<br>".join(messages)
 
 @app.route('/')
 def index():
@@ -10,12 +19,13 @@ def index():
     
 @app.route('/<username>')
 def user(username):
-    """User mini homepage"""
-    return "Hi " + username
+    """Display chat messages"""
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
     
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    """message page"""
-    return "{0}: {1}".format(username, message)
+    """creating a new message and redirecting it to the chat page"""
+    add_messages(username, message)
+    return redirect(username)
     
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
